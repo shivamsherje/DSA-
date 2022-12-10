9ffc35f6b01325e4a7a286454eed1ce3122ba124
shivamsherje <sherjes49@gmail.com>
2022-12-10 22:37:23 +0530

    new

---
+++ Selection sort/basic sorting.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let temp = 0;
+      if (arr[j] < arr[i]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  let bag = "";
+  for (let i = 0; i < arr.length; i++) {
+    bag = bag + arr[i] + " ";
+  }
+  console.log(bag);
+}
+selectionSort([3, 5, 0, 9, 8]);
